453b96f37bff20343e5c4e2050d2ec036fa41c6e
yusaku tada <23249543+erines@users.noreply.github.com>
2025-09-05 18:47:28 +0900

    Update company information on page-company.php: changed establishment date format, revised business activities, and updated website link. Added new section for 株式会社ピー・アンド・ジェイ with detailed company information.

--- p-job.jp/page-company.php
+++ p-job.jp/page-company.php
@@ -14,7 +14,7 @@
           </dl>
           <dl class="companyInfo__list">
               <dt class="companyInfo__term">設立</dt>
-              <dd class="companyInfo__description">平成14年12月</dd>
+              <dd class="companyInfo__description">2002年 12月</dd>
           </dl>
           <dl class="companyInfo__list">
               <dt class="companyInfo__term">本社</dt>
@@ -23,16 +23,15 @@
           <dl class="companyInfo__list">
               <dt class="companyInfo__term">事業内容</dt>
               <dd class="companyInfo__description">
-                労働者派遣事業（般）09-300171<br>
-                職業紹介事業&emsp;09-ユ-300089<br>
-                アウトソーシング（業務委託請負）事業<br>
-                スクール（求職者支援訓練）事業
+              求人広告事業<br>
+              教育訓練事業<br>
+              警備事業
               </dd>
           </dl>
           <dl class="companyInfo__list">
               <dt class="companyInfo__term">HP</dt>
               <dd class="companyInfo__description">
-                <a href="https://pluswork.work/" target="_blank">https://pluswork.work/</a>
+                <a href="https://pluswork.jp/" target="_blank">https://pluswork.jp/</a>
               </dd>
           </dl>
       </div>
@@ -41,6 +40,40 @@
       </div>
     </div>
   </section>
+
+<section class="company marginTop-100-48">
+    <div class="company__inner inner">
+      <h1 class="company__title pageTitle pageTitle--withoutIcon">運営会社情報</h1>
+      <div class="company__information companyInfo">
+          <dl class="companyInfo__list">
+              <dt class="companyInfo__term">会社名</dt>
+              <dd class="companyInfo__description">株式会社ピー・アンド・ジェイ</dd>
+          </dl>
+          <dl class="companyInfo__list">
+              <dt class="companyInfo__term">代表者</dt>
+              <dd class="companyInfo__description">宮田直樹</dd>
+          </dl>
+          <dl class="companyInfo__list">
+              <dt class="companyInfo__term">設立</dt>
+              <dd class="companyInfo__description">2024年 1⽉</dd>
+          </dl>
+          <dl class="companyInfo__list">
+              <dt class="companyInfo__term">本社</dt>
+              <dd class="companyInfo__description">〒323-0032&nbsp;栃木県小山市天神町1丁目9番9号</dd>
+          </dl>
+          <dl class="companyInfo__list">
+              <dt class="companyInfo__term">事業内容</dt>
+              <dd class="companyInfo__description">
+              人材派遣事業（派09-300498）<br>
+              有料職業紹介事業（09-ユ-300331）<br>
+              業務請負業<br>
+              外国人特定技能・技能実習制度導入支援
+              </dd>
+          </dl>
+      </div>
+    </div>
+  </section>
+
   <div class="breadcrumb breadcrumb--sp marginTop-40-0">
       <?php
        if ( function_exists( 'bcn_display' ) ) {
